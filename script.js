47c6ea8cc945f92108b86ff2d0ff6feac06ff1bd
hikikomori4308 <165127892+hikikomori4308@users.noreply.github.com>
2024-10-21 01:21:17 +0300

    script.js

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
     event.preventDefault();
     
     const number = document.getElementById('numberInput').value.trim();
+    const dataType = document.getElementById('dataType').value;
     
     fetch('data.txt')
         .then(response => response.text())
         .then(data => {
             const lines = data.split('\n');
-            let result = 'Номер не найден';
+            let result = 'Данные не найдены';
 
             lines.forEach(line => {
                 const parts = line.split(','); // Предполагается, что данные разделены запятой
-                if (parts[0].trim() === number) { // Сравнение с первым элементом строки (номер)
-                    result = line; // вывод всей строки с данным номером
+                
+                // Проверяем тип данных в зависимости от выбранной опции
+                if ((dataType === 'phone' && parts[0].trim() === number) || 
+                    (dataType === 'passport' && parts[1].trim() === number) || 
+                    (dataType === 'inn' && parts[2].trim() === number) || 
+                    (dataType === 'telegram' && parts[3].trim() === number) || 
+                    (dataType === 'vk' && parts[4].trim() === number) || 
+                    (dataType === 'email' && parts[5].trim() === number)) {
+                    result = line; // вывод всей строки с данными
                 }
             });
 
